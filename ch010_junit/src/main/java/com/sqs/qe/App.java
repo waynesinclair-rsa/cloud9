2fd61b5145b9c98231d5eb1571c23c03cc9e46e2
James <james.hart@sqs.com>
2018-06-16 12:39:16 +0200

    Initial commit of multi module project

---
+++ ch010_junit/src/main/java/com/sqs/qe/App.java
@@ -0,0 +1,13 @@
+package com.sqs.qe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
